888e50b1634151ffa9863e16878852b6ad895e91
Harry Oh <harry@5004.pe.kr>
2020-07-05 22:53:16 +0900

    Handling multi data

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,14 +1,19 @@
 'use strict';
 
-const express    = require('express');
-const bodyParser = require('body-parser');
-const morgan     = require('morgan');
-const config     = require('./environment');
-const app        = express();
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      morgan = require('morgan'),
+      Queue = require('bull');
+
+const config = require('./environment');
+
+const app = express();
+const reqQueue = new Queue('SendRequest', `redis://${config.REDIS_HOST}:6379`);
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(require('method-override')());
 
 const port = process.env.PORT || 50080;
 
@@ -16,7 +21,6 @@ var router = express.Router();
 router.use((req, res, next) => {
   // Log
   // console.log(req.host, req.port, req.url);
-  console.log(config.REDIS_HOST)
   next();
 });
 
@@ -24,25 +28,25 @@ router.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
 
-router.route('/q')
-  // .post((req, res) => {
-  //   var bear = new Bear();
-  //   bear.name = req.body.name;
 
-  //   bear.save(function(err) {
-  //     if (err)
-  //       res.send(err);
-
-  //     res.json({ message: 'Bear created!' });
-  //   });
-  // })
-
-  .get((req, res) => {
-    res.json({ message: 'q list' });
-  });
-
-const Queue      = require('bull');
-const reqQueue = new Queue('SendRequest', `redis://${config.REDIS_HOST}:6379`);
+// const sampleData = [
+//   {
+//     "dbid": "1",
+//     "fromAddress": "11111",
+//     "toAddress": "222222",
+//     "balance": "0.1"
+//   }, {
+//     "dbid": "2",
+//     "fromAddress": "11111",
+//     "toAddress": "222222",
+//     "balance": "0.1"
+//   }, {
+//     "dbid": "3",
+//     "fromAddress": "11111",
+//     "toAddress": "222222",
+//     "balance": "0.1"
+//   }
+// ]
 
 router.route('/job')
   .get((req, res) => {
@@ -50,15 +54,28 @@ router.route('/job')
   })
 
   .post(async (req, res) => {
-    const job = await reqQueue.add(req.body);
-    console.log(job);
+    let job;
+    let joblist=[];
+    if (!req.body.length) {
+      return res.json(500, {
+        message: 'Error: Data format are wrong!'
+      })
+    }
+
+    for (var i=0; i<req.body.length; i++) {
+      job = await reqQueue.add(req.body[i]);
+      joblist.push(job.id);
+    }
+
     res.json({
-      message: `create new queue(id:${job.id})`,
-      data: job.data
+      message: `create new queues(len: ${req.body.length})`,
+      joblist: joblist.toString(),
+      data: req.body
     });
   })
 
 app.use('/api', router);
 
-app.listen(port);
-console.log('Magic happens on port ' + port);
+var server = app.listen( port, () => {
+  console.log('Listening on port ' + server.address().port);
+});
