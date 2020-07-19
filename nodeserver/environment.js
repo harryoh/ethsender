d0cd75fafdbdb2170fffb04c3ca7313c08683927
Harry Oh <harry@5004.pe.kr>
2020-07-20 00:35:02 +0900

    Add Database

--- nodeserver/environment.js
+++ nodeserver/environment.js
@@ -2,6 +2,13 @@ const environment = {
   common: {
     REDIS_PORT: process.env.REDIS_PORT || 6379,
     REDIS_HOST: process.env.REDIS_HOST || 'redis',
+    DB: {
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'willsoft0780!@',
+      database: 'oneethernet'
+    }
   },
   development: {
   },
--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,12 +1,29 @@
 'use strict';
 
 const express = require('express'),
+      mariadb = require('mariadb'),
       bodyParser = require('body-parser'),
       morgan = require('morgan'),
       Queue = require('bull'),
       Arena = require('bull-arena');
 
 const config = require('./environment');
+
+const STATUS = {
+  EMPTY: 0,
+  PREPARE: 1,
+  PENDING: 2,
+  COMPLETE: 3,
+  ERROR: 5
+};
+
+const pool = mariadb.createPool({
+  host: config.DB.host,
+  port: config.DB.port,
+  user: config.DB.user,
+  password: config.DB.password,
+  database: config.DB.database
+});
 
 const app = express();
 const REDIS_URL = `redis://${config.REDIS_HOST}:${config.REDIS_PORT}`;
@@ -29,55 +46,138 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+router.route('/transfer')
+.post(async (req, res) => {
+  let job;
+  let joblist=[];
+  if (!req.body.length) {
+    return res.status(500).json({
+      Error: 'Data format are wrong!'
+    });
+  }
+
+  for (var i=0; i<req.body.length; i++) {
+    job = await reqQueue.add(req.body[i]);
+    joblist.push(job.id);
+  }
+
+  return res.status(201).json({
+    message: `create new queues(len: ${req.body.length})`,
+    joblist: joblist.toString(),
+    data: req.body
+  });
+})
+
+router.route('/request/:id')
+.get(async (req, res) => {
+  let dbconn, rows;
+  try {
+    dbconn = await pool.getConnection();
+    rows = await dbconn.query('SELECT * FROM wallet_send_list WHERE no=?', [req.params.id]);
+    if (!rows.length) {
+      throw(new Error(`The request(no:${req.params.id}) was not found.`));
+    }
+  } catch (err) {
+    if (dbconn) dbconn.end();
+    return res.status(500).json({
+      Error: err.message
+    });
+  }
+  dbconn.end();
+  return res.status(200).json(rows[0]);
+})
+.put(async (req, res) => {
+  let dbconn, rows;
+  try {
+    dbconn = await pool.getConnection();
+    delete req.body.no;
+    const columns = Object.keys(req.body);
+    const values = Object.values(req.body);
+    values.push(req.params.id);
+    const sql = "UPDATE wallet_send_list SET " + columns.join("=?, ") + "=? WHERE no=?";
+    rows = await dbconn.query(sql, values);
+  } catch (err) {
+    if (dbconn) dbconn.end();
+    return res.status(500).json({
+      Error: err.message
+    });
+  }
+  dbconn.end();
+  return res.status(200).json({ message: 'OK' });
 });
 
+router.route('/tx/pending')
+.get(async (req, res) => {
+  let dbconn, rows;
+  try {
+    dbconn = await pool.getConnection();
+    rows = await dbconn.query('SELECT * FROM wallet_send_list WHERE txid != "" and status=?', [STATUS.PENDING]);
+  } catch (err) {
+    if (dbconn) dbconn.end();
+    return res.status(500).json({
+      Error: err.message
+    });
+  }
+  dbconn.end();
+  return res.status(200).json(rows);
+})
 
-// const sampleData = [
-//   {
-//     "dbid": "1",
-//     "fromAddress": "11111",
-//     "toAddress": "222222",
-//     "balance": "0.1"
-//   }, {
-//     "dbid": "2",
-//     "fromAddress": "11111",
-//     "toAddress": "222222",
-//     "balance": "0.1"
-//   }, {
-//     "dbid": "3",
-//     "fromAddress": "11111",
-//     "toAddress": "222222",
-//     "balance": "0.1"
-//   }
-// ]
+router.route('/tx/error')
+.get(async (req, res) => {
+  let dbconn, rows;
+  try {
+    dbconn = await pool.getConnection();
+    rows = await dbconn.query('SELECT * FROM wallet_send_list WHERE txid != "" and status=?', [STATUS.ERROR]);
+  } catch (err) {
+    if (dbconn) dbconn.end();
+    return res.status(500).json({
+      Error: err.message
+    });
+  }
+  dbconn.end();
+  return res.status(200).json(rows);
+})
 
-router.route('/transfer')
-  .get((req, res) => {
-    res.json({ message: 'get id:' + req.params.id });
-  })
-
-  .post(async (req, res) => {
-    let job;
-    let joblist=[];
-    if (!req.body.length) {
-      return res.json(500, {
-        message: 'Error: Data format are wrong!'
+router.route('/tx/:hash')
+.get(async (req, res) => {
+  let dbconn, rows;
+  try {
+    dbconn = await pool.getConnection();
+    rows = await dbconn.query('SELECT * FROM wallet_send_list WHERE txid=?', [req.params.hash]);
+    if (!rows.length) {
+      dbconn.end();
+      return res.status(404).json({
+        message: `The request(hash:${req.params.hash}) was not found.`
       })
     }
-
-    for (var i=0; i<req.body.length; i++) {
-      job = await reqQueue.add(req.body[i]);
-      joblist.push(job.id);
-    }
-
-    res.json({
-      message: `create new queues(len: ${req.body.length})`,
-      joblist: joblist.toString(),
-      data: req.body
+  } catch (err) {
+    if (dbconn) dbconn.end();
+    return res.status(500).json({
+      Error: err.message
     });
-  })
+  }
+  dbconn.end();
+  return res.status(200).json(rows[0]);
+})
+.put(async (req, res) => {
+  let dbconn, rows;
+  try {
+    dbconn = await pool.getConnection();
+    delete req.body.txid;
+    const columns = Object.keys(req.body);
+    const values = Object.values(req.body);
+    values.push(req.params.hash);
+    const sql = "UPDATE wallet_send_list SET " + columns.join("=?, ") + "=? WHERE txid=?";
+    rows = await dbconn.query(sql, values);
+  } catch (err) {
+    if (dbconn) dbconn.end();
+    return res.status(500).json({
+      Error: err.message
+    });
+  }
+  dbconn.end();
+  return res.status(200).json({ message: 'OK' });
+});
 
 app.use('/api', router);
 
